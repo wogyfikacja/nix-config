92f5ad5fd15916897ee38fe913ce43947aa4c8b0
installer <installer@gmail.com>
2024-11-20 04:24:21 +0100

    initial commit

---
+++ home-manager/home.nix
@@ -0,0 +1,122 @@
+# This is your home-manager configuration file
+# Use this to configure your home environment (it replaces ~/.config/nixpkgs/home.nix)
+{
+  inputs,
+  outputs,
+  lib,
+  config,
+  pkgs,
+  ...
+}: {
+  # You can import other home-manager modules here
+  imports = [
+    # If you want to use modules your own flake exports (from modules/home-manager):
+    # outputs.homeManagerModules.example
+
+    # Or modules exported from other flakes (such as nix-colors):
+    # inputs.nix-colors.homeManagerModules.default
+
+    # You can also split up your configuration and import pieces of it here:
+    # ./nvim.nix
+  ];
+  nixpkgs = {
+    # You can add overlays here
+    
+    overlays = [
+      # Add overlays your own flake exports (from overlays and pkgs dir):
+      outputs.overlays.additions
+      outputs.overlays.modifications
+      outputs.overlays.unstable-packages
+
+      # You can also add overlays exported from other flakes:
+      # neovim-nightly-overlay.overlays.default
+
+      # Or define it inline, for example:
+      # (final: prev: {
+      #   hi = final.hello.overrideAttrs (oldAttrs: {
+      #     patches = [ ./change-hello-to-hi.patch ];
+      #   });
+      # })
+    ];
+    # Configure your nixpkgs instance
+    config = {
+      # Disable if you don't want unfree packages
+      allowUnfree = true;
+      permittedInsecurePackages = [
+                "electron-25.9.0"
+              ];
+  };
+};
+  home.username = "wogyfikacja";
+  home.homeDirectory = "/home/wogyfikacja";
+
+   home.pointerCursor = 
+    let 
+      getFrom = url: hash: name: {
+          gtk.enable = true;
+          x11.enable = true;
+          name = name;
+          size = 48;
+          package = 
+            pkgs.runCommand "moveUp" {} ''
+              mkdir -p $out/share/icons
+              ln -s ${pkgs.fetchzip {
+                url = url;
+                hash = hash;
+              }} $out/share/icons/${name}
+          '';
+        };
+    in
+      getFrom 
+        "https://github.com/ful1e5/fuchsia-cursor/releases/download/v2.0.0/Fuchsia-Pop.tar.gz"
+        "sha256-BvVE9qupMjw7JRqFUj1J0a4ys6kc9fOLBPx2bGaapTk="
+        "Fuchsia-Pop";
+  home.packages = [
+    pkgs.firefox
+    pkgs.spotify
+    pkgs.vscode-fhs
+    pkgs.fastfetch
+    pkgs.zsh
+    pkgs.vesktop
+    pkgs.direnv
+    pkgs.python3
+    pkgs.cargo
+    pkgs.pciutils
+    pkgs.bibata-cursors
+    pkgs.mdslides
+    pkgs.obsidian
+    pkgs.lutris
+    pkgs.wine
+    pkgs.winetricks
+    pkgs.deluge
+    pkgs.jdk
+    pkgs.gh
+    pkgs.maven
+    pkgs.sqlite
+    pkgs.flatpak
+    pkgs.element-desktop
+    pkgs.retroarch
+  ];
+  # This value determines the Home Manager release that your
+  # configuration is compatible with. This helps avoid breakage
+  # when a new Home Manager release introduces backwards
+  # incompatible changes.
+  #
+  # You can update Home Manager without changing this value. See
+  # the Home Manager release notes for a list of state version
+  # changes in each release.
+  services.flatpak.enable = true;
+  programs.git = {
+    enable = true;
+    userName = "wogyfikacja";
+    userEmail = "emil1bakula@gmail.com";
+  }; 
+  # Enable home-manager and git
+  programs.home-manager.enable = true;
+
+  # Nicely reload system units when changing configs
+  systemd.user.startServices = "sd-switch";
+
+  # https://nixos.wiki/wiki/FAQ/When_do_I_update_stateVersion
+  home.stateVersion = "23.11";
+}
